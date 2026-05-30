77e0e80d3a7a54b1407c1378b029752ee8263fce
krisyiser <krisyiser@users.noreply.github.com>
2026-05-29 18:44:04 -0600

    fix: add missing ServiceWorkerRegistration component and sw.js to fix build error

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'papantla-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/hero-b-background.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
